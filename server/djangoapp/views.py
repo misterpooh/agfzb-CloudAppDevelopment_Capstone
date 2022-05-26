ba1768db73545ecb3ca74338d57bd8ddbd05c39d
james <jameslee10@live.com>
2022-05-26 06:04:50 +0000

    Updated views.py

--- server/djangoapp/views.py
+++ server/djangoapp/views.py
@@ -86,6 +86,7 @@ def registration_request(request):
 
 # Update the `get_dealerships` view to render the index page with a list of dealerships
 def get_dealerships(request):
+    context = {}
     if request.method == "GET":
         url = "https://2061e0f3.us-east.apigw.appdomain.cloud/api/dealership"
         # Get dealers from the URL
@@ -93,7 +94,9 @@ def get_dealerships(request):
         # Concat all dealer's short name
         dealer_names = ' '.join([dealer.short_name for dealer in dealerships])
         # Return a list of dealer short name
-        return HttpResponse(dealer_names)
+        #return HttpResponse(dealer_names)
+        context['dealerships'] = dealerships 
+        return render(request, 'djangoapp/index.html', context)
 # Create a `get_dealer_details` view to render the reviews of a dealer
 # def get_dealer_details(request, dealer_id):
 # ...
